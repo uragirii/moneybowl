4b241bd2fef8b11b31011e523a7970ce8f3dfee1
il3ven <vaibhavchanana@yahoo.co.in>
2025-01-03 14:58:23 +0530

    fix posthog init

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,7 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    ui_host: 'https://eu.posthog.com',
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
